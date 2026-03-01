7b94b4cdca2aa41431aae9b1c8c85cd8a672e38e
KHALID EL MENNANI <eelkhalid284@gmail.com>
2026-03-01 13:26:40 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+"""
+Configuration file - Change settings here easily!
+"""
+
+# Window settings
+WINDOW_TITLE = "Aqua Vortex"
+BACKGROUND_COLOR = "black"
+
+# Pen settings
+PEN_COLOR = "aqua"
+PEN_SPEED = 0  # 0 = fastest
+
+# Pattern settings
+NUM_CIRCLES = 170
+CIRCLE_RADIUS_START = -5  # Starting radius
+ROTATION_ANGLE = 5  # Degrees to turn each time
+
+# Optional: Multiple colors
+USE_MULTIPLE_COLORS = False
+COLORS = ["aqua", "cyan", "blue", "purple"]
